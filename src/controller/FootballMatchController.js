56831250963680d53de33e68adb2e0e8f434cabe
primahafiz <primahafiz@gmail.com>
2022-09-27 15:45:09 +0700

    update root endpoint

--- src/controller/FootballMatchController.js
+++ src/controller/FootballMatchController.js
@@ -1,15 +1,18 @@
 const {FootballMatch} = require('../models')
-const {sequelize} = require('../models')
+const {sequelize,Op} = require('../models')
 
 module.exports = {
     async getAllData(req,res){
         const countData = await FootballMatch.count()
-        var num = countData-10
+        var num = countData-15
         if(num<0){
             num=0
         }
         const matchList = await FootballMatch.findAll({
-            offset : num
+            offset : num,
+            where:{
+                [Op.or]: [{StatusMatch: "CANC"}, {StatusMatch: "TBD"}]
+            }
         })
         
         res.send(matchList)
