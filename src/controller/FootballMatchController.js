4bfa23f3031592af04359ef5f6127d7282ebd332
primahafiz <primahafiz@gmail.com>
2022-09-27 21:44:25 +0700

    update root endpoint

--- src/controller/FootballMatchController.js
+++ src/controller/FootballMatchController.js
@@ -4,6 +4,13 @@ const { Op } = require('sequelize')
 
 module.exports = {
     async getAllData(req,res){
+        let data = fs.readFileSync('update.json')
+        let update = JSON.parse(data);
+        if(new Date() - new Date(update.nextStartTime) >= 200000 && new Date()<new Date(update.nextEndTime)){
+            await LiveUpdate.liveUpdate(update.nextIDMatch)
+            update.nextStartTime = (new Date()).getTime()
+            fs.writeFileSync('update.json', JSON.stringify(update))
+        }
         const countData = await FootballMatch.count()
         var num = countData-15
         if(num<0){
